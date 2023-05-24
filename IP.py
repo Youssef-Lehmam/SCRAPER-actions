6fe533e4fc6bd6b94d612a716770039e6bc9f9f5
GHOST <72468238+Youssef-Lehmam@users.noreply.github.com>
2023-05-24 22:05:53 +0100

    Update IP.py

--- IP.py
+++ IP.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if response.status_code == 200:
         ip_info = response.json()
         ip_address = ip_info["ip"]
-        ip_address = "\n- " + datetime.datetime.now().strftime("%H:%M %d/%m/%y") + " ➜ " " + ip_address
+        ip_address = "\n- " + datetime.datetime.now().strftime("%H:%M %d/%m/%y") + " ➜ "  + ip_address
         readme = Path("README.md").read_text(encoding="utf8")
         readme_title = "# Latest IP"
         new_readme = readme[:readme.find(readme_title)]  + f"{ip_address}\n\n"
