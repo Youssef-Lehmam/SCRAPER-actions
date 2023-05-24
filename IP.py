d64528f441b7205dcc33601b16ff6aff361334c7
GHOST <72468238+Youssef-Lehmam@users.noreply.github.com>
2023-05-24 22:02:00 +0100

    Update IP.py

--- IP.py
+++ IP.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         ip_address = ip_info["ip"]
         ip_address = "\n- " + datetime.datetime.now().strftime("%H:%M %d/%m/%y") + " " + ip_address
         readme = Path("README.md").read_text(encoding="utf8")
-        readme_title = "# Latest 84xo Headlines"
+        readme_title = "# Latest IP"
         new_readme = readme[:readme.find(readme_title)] + f"{ip_address}\n\n"
         with open("README.md", "w+") as f:
             f.write(new_readme)
