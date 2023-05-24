58300410d72a332fc92da5ce4cf834f230cb3c47
GHOST <72468238+Youssef-Lehmam@users.noreply.github.com>
2023-05-24 21:59:24 +0100

    Update IP.py

--- IP.py
+++ IP.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import datetime
 import requests
 
 if __name__ == "__main__":
@@ -6,17 +7,8 @@ if __name__ == "__main__":
     if response.status_code == 200:
         ip_info = response.json()
         ip_address = ip_info["ip"]
-
-        # Read existing content from README.md
-        readme_path = Path("README.md")
-        readme_content = readme_path.read_text(encoding="utf8")
-
-        # Create a comment line with the IP address
-        comment_line = f"Added IP: {ip_address}"
-
-        # Append the comment line as a new line
-        new_readme = f"{readme_content.strip()}\n{comment_line}\n"
-
-        # Write updated content to README.md
+        ip_address = "\n- " + datetime.datetime.now().strftime("%H:%M %d.%m.%y") + " " + ip_address
+        readme = Path("README.md").read_text(encoding="utf8")
+        new_readme = f"{ip_address}\n\n"
         with open("README.md", "w+") as f:
             f.write(new_readme)
