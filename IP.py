598d06d9614c3ef1b88476af16775475e85fe0b6
GHOST <72468238+Youssef-Lehmam@users.noreply.github.com>
2023-05-24 21:48:23 +0100

    v2

--- IP.py
+++ IP.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
         ip_info = response.json()
         ip_address = ip_info["ip"]
         readme = Path("README.md").read_text(encoding="utf8")
-        readme_title = "# Latest IP scraped from https://api.ipify.org?format=json"
-        new_readme = readme[:readme.find(readme_title)] + f"{ip_address}\n\n"
+        new_readme = f"{ip_address}\n\n\n"
         with open("README.md", "w+") as f:
             f.write(new_readme)
